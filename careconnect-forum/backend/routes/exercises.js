bda21cb4c17b8d1a82da7d2e306ec97451889fc6
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-31 00:13:51 -0500

    Update exercises.js

---
+++ careconnect-forum/backend/routes/exercises.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const email = req.body.email;
+
+    const newExercise = new Exercise({ username, description, email });
