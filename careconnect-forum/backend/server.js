9dfd646e3ebed072e0100de0325f5133ca121ef6
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-30 22:31:59 -0500

    created .env

---
+++ careconnect-forum/backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`); 
+});
